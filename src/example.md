ccd93b32867a1d3e8ab99ed966d248dce3663240
Artem <rtkushner@gmail.com>
2022-06-28 04:51:35 -0700

    got a better idea of sig/r/w indexes

---
+++ src/example.md
@@ -0,0 +1,41 @@
+https://solscan.io/tx/A3Qa57MkJGSVRK31LXLgkuwRCtv4MBJjNWjtLPZo1uDz7kTtKPDvBuYtXngQ7AWTicYYpabfcfLZyng6Au1MuBG
+
+
+- The general question being if my grasp of this is correct?
+
+```json
+{
+                "message": {
+                    "accountKeys": [
+                        "6aCna9ZopJJUuTijkuKLmd57tnMco8KQBH7J8ydCjT2r", // signed   r/w
+                        "EfKB2E4kYinooGF4BFMWXgS2gZLFeBDQ2hffo9LSSN9V", // signed   r
+                        "ChigE9pK6g4UW3skQnKFAwyGETLzEcS2RYDep77XzmJt", // unsigned r/w
+                        "SysvarS1otHashes111111111111111111111111111",  // unsigned r
+                        "SysvarC1ock11111111111111111111111111111111",  // unsigned r
+                        "Vote111111111111111111111111111111111111111"   // unsigned r
+                    ],
+                    "header": {
+                        "numReadonlySignedAccounts"  : 1,   // <-- of those requiring signatures
+                        "numReadonlyUnsignedAccounts": 3,   // <-- last
+                        "numRequiredSignatures"      : 2    // <-- first
+                    },
+                    "instructions": [
+                        {
+                            "accounts": [
+                                2,
+                                3,
+                                4,
+                                1
+                            ],
+                            "data": "rTDbDtm67JPw9Wo3Q1CoZmTfRQYxDy9k4CaEL5AkEHtF1n7MmYpitzHRci5kiVDDNfQ95xjiwnEYoWyVNe5zvfKYesZjvVcAB7SB4nR5",
+                            "programIdIndex": 5
+                        }
+                    ],
+                    "recentBlockhash": "FfVd6vYQjLNE7GAeoFJtaJYbkWYjR5qVBz3pgyNgTCLy"
+                },
+                "signatures": [
+                    "A3Qa57MkJGSVRK31LXLgkuwRCtv4MBJjNWjtLPZo1uDz7kTtKPDvBuYtXngQ7AWTicYYpabfcfLZyng6Au1MuBG",
+                    "2ULwuU8MQcyhhydtY2QianRXRpxVMU8ZWus9xmLefatp35NyxKN1Ziubg1npczniaoMSViigbxyJwzt7YGwER59P"
+                ]
+            }
+```
--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,26 @@ pub fn str_is_pda(acc: &&str) -> Result<bool, bs58::decode::Error> {
     Ok(Pubkey::new(&bytes).is_on_curve())
 }
 
+
+
+
 #[derive(Default,Debug)]
 pub struct AccountProfile {
+    pub num_entered_as_signed   : u64,
+    pub num_entered_as_readwrite: u64,
+    pub num_entered_as_readonly : u64,
+
+    pub tx_top_mentions       : u64,
+    pub unique_in_instructions: u64,
+    pub all_in_instructions   : u64,
+
     pub is_program: bool,
 
-    pub mentions: u64,
-    pub in_transactions: u64,
-    pub in_instructions: u64,
-    pub call_to: u64,
+    // If it's a program 
+    pub num_call_to      : u64,
+    pub num_input_accs_ix: Vec<u8>,
+    pub arg_data         : Vec<Vec<u8>>,
 
-    pub arg_data_size: u64,
-    pub num_accounts_ix: u64,
-
-    pub num_entered_as_signed: u64,
-    pub num_entered_as_readwrite: u64,
-    pub num_entered_as_readonly: u64,
 }
 
 
@@ -46,7 +51,7 @@ pub fn main() -> io::Result<()> {
 
     for blockpath in rd.iter() {
         let block_acc_profile = 0;
-        let block_accounts = HashMap::<String, AccountProfile>::new();
+        let block_accounts    = HashMap::<String, AccountProfile>::new();
 
         println!("------------------------------------------------------------------");
         println!("Processing blockpath: {}", blockpath.display());
@@ -103,52 +108,57 @@ pub struct DeserializationError{
 }
 
 
-pub fn process_instruction(tx_accs:&Vec<&str>, tx_hm: &mut HashMap<&str,AccountProfile > ,ix:&Value)->Result<u8,DeserializationError>{
 
-    let acc_ids  = ix["accounts"].as_array().ok_or(DeserializationError{msg:"couldn't get accoint_ids".to_string(), ..Default::default()})?
+pub fn process_instruction(tx_accs:&[&str], tx_hm: &mut HashMap<&str,AccountProfile > ,ix:&Value)->Result<(),DeserializationError>{
+// ! Remember to -1 the pid indexes
+
+    let ix_acc_inds  = ix["accounts"].as_array().ok_or(DeserializationError{msg:"couldn't get accoint_ids".to_string(), ..Default::default()})?
     .iter().map(|id| id.as_u64().unwrap()).collect::<Vec<u64>>();
 
-    let ixdata   = ix["data"].as_str().ok_or(DeserializationError{msg:"couldn't get ix data ".to_string(), ..Default::default()})?;
-    let pidindex = ix["programIdIndex"].as_u64().ok_or(DeserializationError{msg:"couldn't get prog index ".to_string(), ..Default::default()})?;
+    let ix_data    = ix["data"].as_str().ok_or(DeserializationError{msg:"couldn't get ix data ".to_string(), ..Default::default()})?;
+    let ix_pid_ind = ix["programIdIndex"].as_u64().ok_or(DeserializationError{msg:"couldn't get prog index ".to_string(), ..Default::default()})?;
 
-
-    // println!("Got accountlist : {:?}", tx_accs);
-    // println!("indexed with pid {} : {:?}", pidindex, tx_accs[pidindex as usize-1]);
-    // acc_ids[pidindex as usize].to_string();
-
-    println!("tx accs : {:?} | pidid :{:?}", tx_accs.len(), pidindex);
-    println!("tx accs : {:?} | pidid :{:?}", tx_accs.len(), pidindex);
-    if (tx_accs.len() == pidindex as usize) {
-        panic!("pidindex out of bounds");
+    for acc_index in ix_acc_inds{
+        println!("{}th account in tx_accs({:?}) is {}", acc_index, tx_accs, tx_accs[acc_index as usize - 1]);
     }
+    
+    // tx_hm.entry().or_insert(Default::default());
 
-    Ok(0)
+
+    Ok(())
 }
 
 
+/// Extracting data from a single transaction
+/// # Arguments
+/// * `tx` - the transaction to extract data from
+/// # Returns
+/// * `Result<HashMap<String, AccountProfile>, DeserializationError>` - the hashmap for this particular tx to be merged with a per-block one
 pub fn tx_extract_accdata (tx:&Value) -> Result<HashMap<String, AccountProfile>, DeserializationError>{
-    // let account_list    = tx["transaction"]["message"]["accountKeys"].as_array().ok_or(DeserializationError{msg:"couldn't get transaction", ..Default::default()})?;
 
-    let mut account_list    = tx["message"]["accountKeys"]
+    let mut account_list: Vec<&str>    = tx["message"]["accountKeys"]
     .as_array().ok_or(DeserializationError{msg:"couldn't get account_list".to_string(), ..Default::default()})?
     .iter().map(|x| x.as_str().unwrap()).collect::<Vec<&str>>();
 
-    // println!(" Acc list is {:#?}", account_list);
+    let mut hm_per_tx  = HashMap::new();
+    for acc in account_list.iter(){
+        hm_per_tx.insert(acc.to_string(), AccountProfile::default());
+        
+    }
+
 
     let ixs             = tx["message"]["instructions"].as_array().ok_or(DeserializationError{msg:"couldn't get instructions".to_string(), ..Default::default()})?;
     // let recentBlockhash = tx["message"]["recentBlockhash"].as_array().ok_or(DeserializationError{msg:"couldn't get recent blockhash".to_string(), ..Default::default()})?;
     // let header          = tx["message"]["header"].as_array().ok_or(DeserializationError{msg:"couldn't get the header".to_string(), ..Default::default()})?;
 
 
-    let mut hm_per_tx  = HashMap::new();
-    // for acc in account_list{
-    //     hm_per_tx.insert(acc.to_string(), AccountProfile::default());
-    // }
+
 
 
     for ix in ixs {
         // println!("Instruction : {}", ix);
-        process_instruction(&account_list, &mut HashMap::new(), &ix)?;
+        process_instruction(account_list.as_slice(), &mut HashMap::new(), &ix)?;
+        panic!()
     }
 
 
